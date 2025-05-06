21dd7232e731362cb1812c21b23738701b9a73a2
Harper Lu <lyraxiao61@gmail.com>
2025-05-05 19:09:02 -0700

    vgu

---
+++ javascript/sketch3.js
@@ -0,0 +1,39 @@
+let cloudOneX = 50;
+let count = 0;
+function setup() {
+    createCanvas(1277, 700);
+  
+    }
+  
+ 
+    function draw() {
+    background('navy');
+
+    fill(255);
+    stroke(0);
+    circle(1221, 50, 100);
+
+    stroke("navy");   
+    fill("navy");
+    circle(1191,50,100);
+
+    stroke(0);
+    fill(80);
+    triangle(-400,500,100,200,300,500);
+    triangle(30,500,600,100,1000,500);
+    triangle(-40,500,300,250, 600,500);
+    triangle(400,500,900,100,1317,500);
+    triangle(900,500,1200,900,1600,500);
+
+    fill('rgb(74, 129, 74)');
+  rect(0,500, 1277, 200);
+
+  fill(255);
+  ellipse(cloudOneX, 50, 80, 40);
+  cloudOneX = count % width
+
+    fill(255)
+    text(`${mouseX}, ${mouseY}`, 20, 20);
+    count = count + 2;
+    
+    }
