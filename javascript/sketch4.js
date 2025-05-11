117c783bc216429dc0262bf13c32ed4c70f692fa
Harper Lu <lyraxiao61@gmail.com>
2025-05-10 18:21:09 -0700

    .jvh

---
+++ javascript/sketch4.js
@@ -0,0 +1,16 @@
+    let sunHeight = 600;
+    let redVal = 0;
+    let greenVal = 0;
+    function setup() {
+    createCanvas(1277, 700);
+  
+    }
+    function draw() {
+    background(redVal, greenVal, 0);
+
+    fill(255, 135, 5, 60);
+    circle(300, sunHeight, 180);
+    fill(255, 100, 0, 100);
+    circle(300, sunHeight, 140);
+
+    }
