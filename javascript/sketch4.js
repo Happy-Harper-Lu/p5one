56eb614a15f2da30144baaf0f52c23f7c5090c62
Harper Lu <lyraxiao61@gmail.com>
2025-05-10 18:28:19 -0700

    luy

--- javascript/sketch4.js
+++ javascript/sketch4.js
@@ -9,8 +9,23 @@
     background(redVal, greenVal, 0);
 
     fill(255, 135, 5, 60);
-    circle(300, sunHeight, 180);
+    circle(638, sunHeight, 180);
     fill(255, 100, 0, 100);
-    circle(300, sunHeight, 140);
+    circle(640, sunHeight, 140);
+    
+    fill(110, 50, 18);
+    triangle(200, 400, 520, 253, 800, 400);
+    fill(110,95,20);
+    triangle(200,400,520,253,350,400);  
+
+    fill(150, 75, 0);
+    triangle(-100, 400, 150, 200, 400, 400);
+    fill(100, 50, 12);
+    triangle(-100, 400, 150, 200, 0, 400); 
+
+    fill(150, 100, 0);
+    triangle(200, 400, 450, 250, 800, 400);
+    fill(120, 80, 50);
+    triangle(200, 400, 450, 250, 300, 400);
 
     }
